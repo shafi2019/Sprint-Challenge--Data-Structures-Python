38a79a5a602d1c023812a8bc2b9c467559c6a16d
Shafi Masoumi <masoumi2017@icould.com>
2020-08-01 10:24:10 -0700

    ring_buffer done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.head = 0
 
     def append(self, item):
-        pass
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+        else:
+            if self.head == self.capacity:
+                self.head = 0
+            self.data[self.head] = item
+        self.head += 1
+            # self.data[self.head] = item
+            # self.head = (self.head + 1) self.capacity
 
     def get(self):
-        pass
+        return self.data
